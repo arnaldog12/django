a75b31f4d2c5aa4e7280813c80186c25041bd6e9
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2017-10-31 19:03:25 -0300

    Levantamento de erros, remoção de dependencias e url namespacing

--- mysite/pools/views.py
+++ mysite/pools/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+
 from .models import Question
 
 def index(request):
@@ -16,7 +19,8 @@ def index(request):
     # return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question {}".format(question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'pools/details.html', {'question':question})
 
 def results(request, question_id):
     return HttpResponse("You're looking at the results of question {}".format(question_id))
