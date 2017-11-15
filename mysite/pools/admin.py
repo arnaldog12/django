e806722215d1ac7f2f0b76d490f7034e49dc76f0
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2017-11-15 09:20:58 -0300

    Parte 7 - Personalize o formulário do site de administração

--- mysite/pools/admin.py
+++ mysite/pools/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
-from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+'''
+admin.StackedInline: Empilha os campos do modelo Choice
+admin.TabularInline: Coloca os campos do modelos Choice em uma única linha
+'''
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text'] # define a ordem que os campos aparecem
+
+    # divide o formulário em grupos
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date']})
+    ]
+    inlines = [ChoiceInline]
+
+# usuário: admin senha: adminadmin
+admin.site.register(Question, QuestionAdmin)
+#admin.site.register(Choice)
